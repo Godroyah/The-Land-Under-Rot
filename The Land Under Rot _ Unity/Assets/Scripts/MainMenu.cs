a2659a6658e67aea2bf994ed0095d981cb600df7
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2020-01-31 08:58:35 -0500

    Programmers
    Rebuilding branching system

---
+++ The Land Under Rot _ Unity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    
+    //-----------------------------------------------------------------
+
+    public bool testing;
+
+    //-----------------------------------------------------------------
+    //Only for testing both Main and Pause Menu in same scene;
+    //Turn this off on MainMenuHolder AND GameController gameobjects
+    //once MainMenu has its own scene
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Time.timeScale != 1)
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        if(testing)
+        {
+            mainMenu.SetActive(false);
+        }
+        else
+        {
+            SceneManager.LoadScene("Tutorial_Area");
+        }
+        Cursor.visible = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
