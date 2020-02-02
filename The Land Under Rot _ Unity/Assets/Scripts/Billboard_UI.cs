0f06d93b9cda44c92df8e4a48e1001a28b1472de
Nick <ncazmay@student.fitchburgstate.edu>
2020-02-01 19:05:23 -0500

    Button Prompts Test 2
    Figured out issue with "billboarding" attempt for visual representation of prompt in 3d space; canvas was getting squashed as it rotated to follow camera because the parent empty game object's scale was not equivalent (ex. x = 1, y = 1, z = 1, etc.) This has been resolved and prompts can have their text customized depending on object type (ex. Sign, NPC, Bark, etc.)
    
    SignTest Prefab now contains both the player detection script as well as the button prompt itself along with its billboard script.

--- The Land Under Rot _ Unity/Assets/Scripts/Billboard_UI.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/Billboard_UI.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Billboard_UI : MonoBehaviour
 {
+    //-----WARNING! Make sure XYX Scale you parent the Button_Prompt to are equivalent! 
+    //-----(ex. x = 1, y = 1, z = 1 OR x = 2, y = 2, z = 2, ETC.)
+
+
+
     public Transform camTransform;
+    //private Canvas thisCanvas;
+    //private Camera eventCam;
 
     Quaternion startRotation;
 
     // Start is called before the first frame update
     void Start()
     {
+        //playerCam = Camera.main;
+        //camTransform = playerCam.transform;
+        //thisCanvas = GetComponent<Canvas>();
+        //thisCanvas.worldCamera = playerCam;
+        
         startRotation = transform.rotation;
     }
 
@@ -19,6 +31,14 @@ public class Billboard_UI : MonoBehaviour
     {
         //transform.LookAt(camTransform.rotation.x, camTransform.rotation.y, 0);
         //transform.rotation = Quaternion.LookRotation(-camTransform.forward, camTransform.up);
+
+        //----------------------------------------------------------------------------------------
+        //-------Multiply the start rotation of Button Prompt with that of the camera-------------
         transform.rotation = camTransform.rotation * startRotation;
+        //----------------------------------------------------------------------------------------
+        //-------This will keep the UI facing the camera at all times (Billboarding)
+
+
+        //transform.localRotation = camTransform.rotation * startRotation;
     }
 }
--- The Land Under Rot _ Unity/Assets/Scripts/Button_Prompter.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/Button_Prompter.cs
@@ -1,21 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Button_Prompter : MonoBehaviour
 {
     public GameObject buttonPromptUI;
 
+    public bool NPC;
+
+    public bool Sign;
+
+    public bool Bark;
+
+    public bool EyeGazer;
+
+    private string objectTag;
+
+    public string signText;
+
+    public string npcText;
+
+    public string barkText;
+
+    public string eyeGazerText;
+
+    private Text promptText;
+
     // Start is called before the first frame update
     void Start()
     {
+        objectTag = gameObject.tag;
+        promptText = buttonPromptUI.GetComponentInChildren<Text>();
         buttonPromptUI.SetActive(false);
+        if(NPC)
+        {
+            promptText.text = npcText;
+        }
+        else if(Sign)
+        {
+            promptText.text = signText;
+        }
+        else if(Bark)
+        {
+            promptText.text = barkText;
+        }
+        else if(EyeGazer)
+        {
+            promptText.text = eyeGazerText;
+        }
     }
 
     void OnTriggerEnter(Collider player)
     {
         if(player.gameObject.tag == "Player")
         {
+            //Instantiate(buttonPromptUI, transform.position, transform.rotation);
             buttonPromptUI.SetActive(true);
         }
     }
