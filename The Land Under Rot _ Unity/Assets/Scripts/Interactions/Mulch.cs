9068e8047cbbf3a63b115dca49b3fddf9f45d738
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2020-02-03 17:09:29 -0500

    Mulch

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Interactions/Mulch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mulch : Interactable
+{
+    public GameObject[] objsToSpawn;
+
+    public float spawnJumpHeight = 1f;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        foreach (GameObject obj in objsToSpawn)
+        {
+            GameObject temp = Instantiate(obj, transform);
+            temp.transform.position = transform.position;
+
+            Rigidbody rb;
+            rb = temp.GetComponent<Rigidbody>();
+
+            if (rb == null) // TODO: Mulch Spawn Rigidbodies
+                rb = temp.AddComponent<Rigidbody>();
+
+            temp.transform.rotation = Quaternion.Euler(0, Random.Range(-180, 180), 0);
+
+            rb.AddForce((Vector3.left / 3f) + Vector3.up * spawnJumpHeight);
+        }
+    }
+}
