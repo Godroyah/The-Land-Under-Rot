429085bfca53992db9717719b4d0002e50f9b796
Nick <ncazmay@student.fitchburgstate.edu>
2020-02-04 09:22:02 -0500

    Re-adding reverted scripts

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Checkpoints/RegionSpawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionSpawn : MonoBehaviour
+{
+    public Transform playerSpawn;
+    public Transform[] spawnPoints;
+    //private List<float> spawnDistance;
+    
+    private float shortestDist;
+    private float currentDist;
+
+    public bool activeSpawnArea;
+
+    public PlayerController playerController;
+    public GameObject gamePlayer;
+    public GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameController.areaSpawnCalc == true)
+        {
+            for(int i = 0; i <= spawnPoints.Length; i++)
+            {
+                currentDist = Vector3.Distance(gamePlayer.transform.position, spawnPoints[i].position);
+                if(currentDist < shortestDist)
+                {
+                    currentDist = shortestDist;
+                    playerSpawn = spawnPoints[i].transform;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        activeSpawnArea = true;
+        gameController.areaSpawnCalc = true;
+        gamePlayer = player.gameObject;
+        gamePlayer.GetComponent<PlayerController>().currentSpawn = playerSpawn; 
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        activeSpawnArea = false;
+    }
+
+
+}
---
+++ The Land Under Rot _ Unity/Assets/Scripts/Checkpoints/SingleSpawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleSpawn : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public Transform playerSpawn;
+    public GameController gameController;
+    //public Transform previousSpawn;
+    //private PlayerController playerController;
+
+    public bool activeCorridor;
+
+    private void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+        if(playerSpawn == null)
+        {
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        if(playerSpawn != spawnPoint)
+        {
+            activeCorridor = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if(player.CompareTag("Player"))
+        {
+            gameController.areaSpawnCalc = false;
+            //previousSpawn = player.GetComponent<PlayerController>().currentSpawn;
+            player.GetComponent<PlayerController>().currentSpawn = spawnPoint;
+            activeCorridor = true;
+        }
+    }
+}
