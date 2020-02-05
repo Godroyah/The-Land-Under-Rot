d86d6e991dc5d53f72aa7120eea8c13b2ced036b
Nick <ncazmay@student.fitchburgstate.edu>
2020-02-05 13:03:42 -0500

    Minor Script Re-Organize

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Player/CamControl.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    public Camera myCamera;
+    public GameObject target;
+    public Transform rotationTarget;
+
+    private bool invertPitch = false;
+    public float sensitivity_X = 10f;
+    public float sensitivity_Y = 10f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    public Vector2 pitchMinMax = new Vector2(-30, 85);
+    public float acceleration = .12f;
+
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw;
+    float pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (myCamera == null)
+        {
+            Debug.LogWarning("No assigned camera found. Defaulting to MAIN");
+            myCamera = Camera.main;
+        }
+
+        yaw = transform.eulerAngles.y;
+        pitch = transform.eulerAngles.x;
+        currentRotation = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.transform.position;
+
+        // TODO: JANK rotation correction
+        // Seems like over correction to me
+        rotationTarget.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Mouse X");
+        verticalInput = Input.GetAxis("Mouse Y");
+    }
+
+    private void Move()
+    {
+        // <<-----------------------------------------------------------------------------**
+        // Get input data and clamp 
+        // **---------------------------------------------**
+
+        yaw += Input.GetAxis("Mouse X") * sensitivity_X;
+
+        if (invertPitch)
+        {
+            pitch += Input.GetAxis("Mouse Y") * sensitivity_Y;
+        }
+
+        else
+        {
+            pitch += Input.GetAxis("Mouse Y") * -sensitivity_Y;
+        }
+
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        // **----------------------------------------------------------------------------->>
+
+
+        // <<-----------------------------------------------------------------------------**
+        // Calculate rotation and apply to camera
+        // **---------------------------------------------**
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, acceleration);
+
+        transform.eulerAngles = currentRotation;
+
+        // **----------------------------------------------------------------------------->>
+    }
+}
---
+++ The Land Under Rot _ Unity/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Components")]
+    #region Player Components
+    //public GameObject playerCamera;
+    private Animator animator;
+    public CamControl camControl;
+    private Rigidbody rb;
+    //Will eventually search spawner out by scene
+    public Transform currentSpawn;
+    public Transform playerHolder;
+    #endregion
+
+    [Header("Player Modifiers")]
+    #region Modifiers
+    public int health = 3;
+
+    [Range(1, 20)]
+    public float moveSpeed = 12f;
+
+    [Range(1, 20)]
+    public float jumpStrength = 10f;
+
+    //[Range(0,200), Tooltip("Controls how fast the obj rotates when moving.")]
+    //public float rotateVel = 100;
+    //public float minRotationDelay = 15f;
+
+    [Range(0, 200), Tooltip("Controls how fast the obj rotates when moving.")]
+    public float rotationSpeed = 0.1f;
+
+    //public float deathfadeDelay;
+
+    [Range(0.001f, 5)]
+    public float fallMultiplierFloat = 2f; // TODO: Look at this for GLIDING later
+    #endregion
+
+
+    [Header("Booleans")]
+    #region Bools
+    public bool isDead;
+    //private bool isDead;
+    private bool isGrounded;
+    private bool isHeadBangin;
+    private bool shouldInteract = false;
+    private bool shouldJump = false;
+    #endregion
+
+
+    [Header("Input")]
+    #region Input
+    [Range(0, 1), Tooltip("Cutoff for the value when the input is accepted.")]
+    public float inputDelay = 0.1f;
+    [Range(0, 1), Tooltip("Delay until the next time 'Interact' is available.")]
+    public float interactDelay = 0.1f;
+    private Coroutine interactingCoroutine;
+    [Range(0.001f, 1), Tooltip("Determines how responsive the obj is to movement.")]
+    public float rotationTargetDist = 0.1f;
+    [Range(0, 1), Tooltip("Cutoff distance for when the obj will rotate. This will also be capped by the rotationTargetDist.")]
+    public float rotationCutoff = 0f;
+    [HideInInspector]
+    public Vector2 controlInput;
+    private float horizontalInput;
+    private float verticalInput;
+    private Quaternion targetRotation;
+    private Transform rotationTarget;
+    #endregion
+
+    private Interactable currentTarget;
+
+    //Hash ID
+    private int MovementID = 0;
+
+    private void Awake()
+    {
+        if (!GetComponent<Rigidbody>())
+            Debug.LogWarning("Rigidbody missing on " + gameObject.name);
+        else
+            rb = GetComponent<Rigidbody>();
+
+        if (camControl == null)
+            Debug.LogWarning("Camera Controller Missing!!");
+
+        if (animator != null)
+            animator = GetComponent<Animator>();
+
+        rotationTarget = new GameObject().transform;
+        rotationTarget.transform.parent = gameObject.transform;
+        rotationTarget.name = "RotationTarget$$";
+
+        camControl.rotationTarget = rotationTarget;
+
+        //camControl = playerCamera.GetComponent<CamControl>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: Find Spawn Point
+        if (currentSpawn == null)
+        {
+            currentSpawn = transform;
+        }
+
+        this.transform.position = currentSpawn.position;
+        this.transform.rotation = currentSpawn.rotation;
+
+        //targetRotation = currentSpawn.rotation;
+
+        //forwardVel = turnInput = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+
+
+        // TODO: Limit this to happen if outside of specific range
+        //       Maybe if no input don't move rotationTarget?
+        if (Vector3.Distance(transform.position, rotationTarget.position) > rotationCutoff)
+        {
+            Rotate();
+        }
+
+        if (shouldJump)
+        {
+            rb.AddForce(rotationTarget.position + Vector3.up * jumpStrength * 50);
+            shouldJump = false;
+        }
+
+        //faster falling
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplierFloat - 1) * Time.deltaTime;
+        }
+
+        if (shouldInteract && interactingCoroutine == null)
+        {
+            StartCoroutine(Interacting());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        shouldJump = Input.GetButtonDown("Jump");
+        shouldInteract = Input.GetButtonDown("Interact");
+    }
+
+    IEnumerator Interacting()
+    {
+        if (currentTarget != null)
+        {
+            currentTarget.Interact(); // TODO: Find interactable target
+        }
+
+        yield return new WaitForSeconds(interactDelay);
+        interactingCoroutine = null;
+    }
+
+    private void Move()
+    {
+        // Don't start accepting input until it peaks the inputDelay
+        if (Mathf.Abs(horizontalInput) < inputDelay)
+            horizontalInput = 0;
+        if (Mathf.Abs(verticalInput) < inputDelay)
+            verticalInput = 0;
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+
+        Vector3 tempDir = rotationTarget.TransformDirection(movement * rotationTargetDist); // TODO: Hardcoded Detection Radius
+        rotationTarget.position = tempDir + transform.position;
+
+        movement = rotationTarget.TransformDirection(movement);
+        movement *= moveSpeed * Time.deltaTime;
+
+        transform.position += movement;
+        //rb.AddForce(movement);
+    }
+
+    private void Rotate()
+    {
+        //find the vector pointing from our position to the target
+        Vector3 _direction = (rotationTarget.position - transform.position).normalized;
+
+        //create the rotation we need to be in to look at the target
+        Quaternion _lookRotation = Quaternion.LookRotation(_direction);
+
+        //rotate us over time according to speed until we are in the required rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed); // TODO: Smoother Transitions
+                                                                                                                  // ^^ For smoother transitions the speed should increase if the distance is shorter
+    }
+
+    private void Respawn()
+    {
+        transform.position = currentSpawn.position;
+        transform.rotation = currentSpawn.rotation;
+        rb.velocity = Vector3.zero;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Spawn_Volume"))
+        {
+            Debug.Log("Collider detected?");
+            Spawn_Volume volume = other.GetComponent<Spawn_Volume>();
+
+            //SpawnType spawnType = SpawnType.NONE;
+            SpawnType spawnType = volume.spawnType;
+
+            switch (spawnType)
+            {
+                case SpawnType.NONE:
+                    Debug.LogWarning("SpawnType not set!");
+                    break;
+                case SpawnType.START:
+                    break;
+                case SpawnType.RESPAWN:
+                    currentSpawn = volume.spawnPoint.transform;
+                    break;
+                case SpawnType.KILL:
+                    Respawn();
+                    break;
+                default:
+                    Debug.LogWarning("SpawnType Error.");
+                    break;
+            }
+        }
+    }
+    
+}
+
+//public enum SpawnType { NONE, START, RESPAWN, KILL }
+//public enum SpawnType { NONE, START, SINGLE_RESPAWN, REGION_RESPAWN, SINGLE_KILL, REGION_KILL }
