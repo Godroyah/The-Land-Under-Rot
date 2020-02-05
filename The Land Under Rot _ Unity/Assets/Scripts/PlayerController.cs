3cc088eb78b7e717bdf4795f3f8b1b2ed228b065
Nick <ncazmay@student.fitchburgstate.edu>
2020-02-04 19:23:34 -0500

    Minor PlayerController Update
    Made isDead public for testing; copied and commented of an alternate version of SpawnType enum.

--- The Land Under Rot _ Unity/Assets/Scripts/PlayerController.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/PlayerController.cs
@@ -240,4 +240,4 @@ public class PlayerController : MonoBehaviour
 }
 
 public enum SpawnType { NONE, START, RESPAWN, KILL }
-//public enum SpawnType { NONE, START, RESPAWN, KILL }
+//public enum SpawnType { NONE, START, SINGLE_RESPAWN, REGION_RESPAWN, SINGLE_KILL, REGION_KILL }
