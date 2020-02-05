6cda80b5dffbcd34b06e98b7afb947577492db78
Nick <ncazmay@student.fitchburgstate.edu>
2020-02-05 11:27:21 -0500

    Start, Spawn, and Kill Volume Prefabs

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Checkpoints/Architecture/Code_Holder.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public enum SpawnType { NONE, START, SINGLE_RESPAWN, REGION_RESPAWN, SINGLE_KILL, REGION_KILL }
+
+public class Code_Holder : MonoBehaviour
+    //Spawn_Volume
+{
+    public SpawnType spawnType; // This gives a drop down menu of the spawn types
+
+    public Transform spawnPoint;
+    public Transform playerSpawn;
+    public Transform[] spawnPoints; // Couldn't this simply be multiple spawn volumes?
+    public float[] spawnDistances;
+    public int distanceIndex;
+    public bool activeCorridor;
+
+    /*
+    public bool single_Spawn;
+    public bool region_Spawn;
+    public bool single_Kill_Volume;
+    public bool region_Kill_Volume;
+    */
+
+
+
+    [SerializeField]
+    private float shortestDist;
+
+    public bool activeSpawnArea;
+
+    public GameObject gamePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (spawnType == SpawnType.REGION_RESPAWN)
+        //{
+        //    spawnDistances = new float[spawnPoints.Length];
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (spawnType == SpawnType.REGION_RESPAWN)
+        //{
+        //    if (activeSpawnArea == true)
+        //    {
+        //        for (int i = 0; i < spawnPoints.Length; i++)
+        //        {
+        //            spawnDistances[i] = Vector3.Distance(spawnPoints[i].position, gamePlayer.transform.position);
+
+        //            if (shortestDist > spawnDistances[i])
+        //            {
+        //                distanceIndex = i;
+        //            }
+
+        //            shortestDist = spawnDistances[distanceIndex];
+        //            //playerSpawn = spawnPoints[distanceIndex];
+        //            spawnPoint = spawnPoints[distanceIndex];
+        //        }
+        //    }
+        //}
+        //else if (spawnType == SpawnType.SINGLE_RESPAWN)
+        //{
+        //    if (playerSpawn != spawnPoint)
+        //    {
+        //        activeCorridor = false;
+        //    }
+        //}
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            //if (spawnType == SpawnType.SINGLE_RESPAWN)
+            //{
+            //    player.GetComponent<PlayerController>().currentSpawn = spawnPoint;
+            //    playerSpawn = player.GetComponent<PlayerController>().currentSpawn;
+            //    activeCorridor = true;
+            //}
+            //else if (spawnType == SpawnType.REGION_RESPAWN)
+            //{
+            //    shortestDist = 99999.0f;
+            //    activeSpawnArea = true;
+            //}
+        }
+    }
+
+    private void OnTriggerStay(Collider player)
+    {
+        //if (spawnType == SpawnType.REGION_RESPAWN)
+        //{
+        //    if (activeSpawnArea)
+        //    {
+        //        gamePlayer = player.gameObject;
+        //    }
+        //}
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        //if (spawnType == SpawnType.REGION_RESPAWN)
+        //{
+        //    if (player.CompareTag("Player"))
+        //    {
+        //        activeSpawnArea = false;
+        //    }
+        //}
+    }
+}
--- The Land Under Rot _ Unity/Assets/Scripts/Checkpoints/Spawn_Volume.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/Checkpoints/Spawn_Volume.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SpawnType { NONE, START, SINGLE_RESPAWN, REGION_RESPAWN, SINGLE_KILL, REGION_KILL }
+public enum SpawnType { NONE, START, RESPAWN, KILL}
 
 public class Spawn_Volume : MonoBehaviour
 {
@@ -10,10 +10,15 @@ public class Spawn_Volume : MonoBehaviour
 
     public Transform spawnPoint;
     public Transform playerSpawn;
-    public Transform[] spawnPoints; // Couldn't this simply be multiple spawn volumes?
-    public float[] spawnDistances;
-    public int distanceIndex;
     public bool activeCorridor;
+    public PlayerController playerController;
+
+    //public GameObject gamePlayer;
+    //public bool activeSpawnArea;
+
+    //public Transform[] spawnPoints; // Couldn't this simply be multiple spawn volumes?
+    //public float[] spawnDistances;
+    //public int distanceIndex;
 
     /*
     public bool single_Spawn;
@@ -22,92 +27,106 @@ public class Spawn_Volume : MonoBehaviour
     public bool region_Kill_Volume;
     */
 
-    
+    //[SerializeField]
+    //private float shortestDist;
 
-    [SerializeField]
-    private float shortestDist;
-
-    public bool activeSpawnArea;
-
-    public GameObject gamePlayer;
-
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
-        if(spawnType == SpawnType.REGION_RESPAWN)
+        //if(spawnType == SpawnType.REGION_RESPAWN)
+        //{
+        //    spawnDistances = new float[spawnPoints.Length];
+        //}
+        if(spawnType == SpawnType.START)
         {
-            spawnDistances = new float[spawnPoints.Length];
+            activeCorridor = true;
+            playerSpawn = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().currentSpawn;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(spawnType == SpawnType.REGION_RESPAWN)
+        if (activeCorridor && playerSpawn != spawnPoint)
         {
-            if (activeSpawnArea == true)
-            {
-                for (int i = 0; i < spawnPoints.Length; i++)
-                {
-                    spawnDistances[i] = Vector3.Distance(spawnPoints[i].position, gamePlayer.transform.position);
-
-                    if (shortestDist > spawnDistances[i])
-                    {
-                        distanceIndex = i;
-                    }
-
-                    shortestDist = spawnDistances[distanceIndex];
-                    //playerSpawn = spawnPoints[distanceIndex];
-                    spawnPoint = spawnPoints[distanceIndex];
-                }
-            }
+            activeCorridor = false;
+            playerSpawn = spawnPoint;
         }
-        else if(spawnType == SpawnType.SINGLE_RESPAWN)
-        {
-            if (playerSpawn != spawnPoint)
-            {
-                activeCorridor = false;
-            }
-        }
+        //if (spawnType == SpawnType.RESPAWN)
+        //{
+        //    if (activeCorridor && playerSpawn != spawnPoint)
+        //    {
+        //        activeCorridor = false;
+        //    }
+        //}
+        //if(spawnType == SpawnType.START)
+        //{
+        //    if(playerSpawn != null)
+        //    {
+        //       if(activeCorridor && playerSpawn != spawnPoint)
+        //        {
+        //            activeCorridor = false;
+        //        }
+        //    }
+        //}
+        //else if(spawnType == SpawnType.REGION_RESPAWN)
+        //{
+        //    if (activeSpawnArea == true)
+        //    {
+        //        for (int i = 0; i < spawnPoints.Length; i++)
+        //        {
+        //            spawnDistances[i] = Vector3.Distance(spawnPoints[i].position, gamePlayer.transform.position);
+
+        //            if (shortestDist > spawnDistances[i])
+        //            {
+        //                distanceIndex = i;
+        //            }
+
+        //            shortestDist = spawnDistances[distanceIndex];
+        //            //playerSpawn = spawnPoints[distanceIndex];
+        //            spawnPoint = spawnPoints[distanceIndex];
+        //        }
+        //    }
+        //}
     }
 
     private void OnTriggerEnter(Collider player)
     {
         if (player.CompareTag("Player"))
         {
-            if(spawnType == SpawnType.SINGLE_RESPAWN)
+            if(spawnType == SpawnType.RESPAWN)
             {
-                player.GetComponent<PlayerController>().currentSpawn = spawnPoint;
+                //player.GetComponent<PlayerController>().currentSpawn = spawnPoint;
                 playerSpawn = player.GetComponent<PlayerController>().currentSpawn;
                 activeCorridor = true;
             }
-            else if(spawnType == SpawnType.REGION_RESPAWN)
-            {
-                shortestDist = 99999.0f;
-                activeSpawnArea = true;
-            }
+            //else if(spawnType == SpawnType.REGION_RESPAWN)
+            //{
+            //    shortestDist = 99999.0f;
+            //    activeSpawnArea = true;
+            //}
         }
     }
 
-    private void OnTriggerStay(Collider player)
-    {
-        if(spawnType == SpawnType.REGION_RESPAWN)
-        {
-            if(activeSpawnArea)
-            {
-                gamePlayer = player.gameObject;
-            }
-        }
-    }
+    //private void OnTriggerStay(Collider player)
+    //{
+    //    if(spawnType == SpawnType.REGION_RESPAWN)
+    //    {
+    //        if(activeSpawnArea)
+    //        {
+    //            gamePlayer = player.gameObject;
+    //        }
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider player)
-    {
-        if(spawnType == SpawnType.REGION_RESPAWN)
-        {
-            if (player.CompareTag("Player"))
-            {
-                activeSpawnArea = false;
-            }
-        }
-    }
+    //private void OnTriggerExit(Collider player)
+    //{
+    //    if(spawnType == SpawnType.REGION_RESPAWN)
+    //    {
+    //        if (player.CompareTag("Player"))
+    //        {
+    //            activeSpawnArea = false;
+    //        }
+    //    }
+    //}
 }
