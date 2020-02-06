ead2a011c27e4c6ddffae121dc905191ea9681d5
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2020-02-06 12:41:19 -0500

    Initial Bark and Enemy Interations

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Interactions/Bark.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bark : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+
+        // TODO: Switch from Destroy to Particle/Anim
+        Destroy(gameObject);
+    }
+}
