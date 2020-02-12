996f788fa188428aa097e51d206337ca1a8c1c14
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2020-02-12 11:15:44 -0500

    End_of_Level
    Used to stop progress of the player once the level is completed and stops the player from movement

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/End_of_Level.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(Rigidbody))]
+public class End_of_Level : MonoBehaviour
+{
+    public GameObject endLevelDisplay;
+    GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endLevelDisplay.SetActive(false);
+
+        #region GameController Search
+        GameObject temp = GameObject.Find("@GameController");
+        if (temp != null)
+        {
+            gameController = temp.GetComponent<GameController>();
+
+            if (gameController == null)
+                Debug.LogWarning("@GameController does not have the 'GameController' script!");
+        }
+        else
+            Debug.LogWarning("Could not find GameController.");
+
+        #endregion
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Headbutt"))
+        {
+            endLevelDisplay.SetActive(true);
+            if (gameController!= null && gameController.playerController != null)
+            {
+                gameController.playerController.enabled = false;
+            }
+        }
+    }
+}
