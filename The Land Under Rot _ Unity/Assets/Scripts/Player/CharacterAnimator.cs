38aee5ba14702c481d19312ae788b6b2939c49d9
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2020-02-20 14:58:47 -0500

    Update

--- The Land Under Rot _ Unity/Assets/Scripts/Player/CharacterAnimator.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/Player/CharacterAnimator.cs
@@ -21,57 +21,44 @@ public class CharacterAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #region Jumping
-
-        if (playerController.ShouldJump)
+        if (playerController.enabled == true)
         {
-            animator.SetTrigger("Jump_Button_Trigger");
-        }
+            #region Jumping
 
-        //faster falling
-        if (rb.velocity.y < 0)
-            animator.SetBool("Airborne_Bool", true);
-        //else if (rb.velocity.y > 0 && !playerController.ShouldJump)
-        //animator.SetBool("Airborne_Bool", true);
-
-        if (!playerController.CanJump)
-        {
-            #region Raycast Layer Masking
-            // Bit shift the index of the layer (8) to get a bit mask
-            int layerMask = 1 << 8;
-
-            // This would cast rays only against colliders in layer 8.
-            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-            layerMask = ~layerMask;
-
-            RaycastHit hit;
-            // Does the ray intersect any objects excluding the player layer
-            #endregion
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 5f))
+            if (playerController.ShouldJump)
             {
-                if (hit.distance < landDistance)
-                {
-                    animator.SetBool("Airborne_Bool", false);
-                }
+                animator.SetTrigger("Jump_Button_Trigger");
             }
+
+            //faster falling
+            if (rb.velocity.y < 0 && !playerController.IsGrounded)
+                animator.SetBool("Airborne_Bool", true);
+            //else if (rb.velocity.y > 0 && !playerController.ShouldJump)
+            //animator.SetBool("Airborne_Bool", true);
+
+            if (playerController.IsGrounded)
+            {
+                animator.SetBool("Airborne_Bool", false);
+            }
+
+            if (!animator.GetBool("Airborne_Bool") && (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")))
+                animator.SetTrigger("Run_Button_Trigger");
+
+
+            if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+                animator.SetBool("Holding_Run_Bool", true);
+            else
+                animator.SetBool("Holding_Run_Bool", false);
+
+
+
+            if (playerController.ShouldHeadbutt && playerController.HeadbuttCoroutine == null)
+            {
+                animator.SetTrigger("Headbutt_Button_Trigger");
+            }
+
+            #endregion
         }
 
-        if (!animator.GetBool("Airborne_Bool") && (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")))
-            animator.SetTrigger("Run_Button_Trigger");
-
-
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
-            animator.SetBool("Holding_Run_Bool", true);
-        else
-            animator.SetBool("Holding_Run_Bool", false);
-
-
-
-        if (playerController.ShouldHeadbutt && playerController.HeadbuttCoroutine == null)
-        {
-            animator.SetTrigger("Headbutt_Button_Trigger");
-        }
-
-        #endregion
     }
 }
