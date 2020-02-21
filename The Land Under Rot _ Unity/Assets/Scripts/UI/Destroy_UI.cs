4cd43545c9b55d9401d908d00a15cd0d9cddbca6
Nick <ncazmay@student.fitchburgstate.edu>
2020-02-21 13:42:41 -0500

    Basic Area Title Card
    Canvas and Player Detector prefabs are available in Prefabs > Systems > UI > World UI > AreaTitle.
    
    Canvas is already hooked up to the Player Detector and does not need to be placed in the scene. Implementer need only type what they want the title card to say in the Player Detector' Area Title field and place it where the player will contact it. The canvas then instantiates, plays, and deletes itself.

---
+++ The Land Under Rot _ Unity/Assets/Scripts/UI/Destroy_UI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Destroy_UI : MonoBehaviour
+{
+    public TextMeshProUGUI area_Title;
+
+    public void SetName(string text)
+    {
+        area_Title.text = text;
+    }
+
+    public void DestroyTitle()
+    {
+        Destroy(gameObject);
+    }
+}
