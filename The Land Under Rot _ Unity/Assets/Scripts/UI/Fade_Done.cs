e03547bee18492d78d33b890efb3b024d8373bc8
Nick Cazmay <ncazmay@student.fitchburgstate.edu>
2020-03-02 16:28:03 -0500

    Moved FadeDone Script
    Just moved file

---
+++ The Land Under Rot _ Unity/Assets/Scripts/UI/Fade_Done.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade_Done : MonoBehaviour
+{
+
+    public bool fadeOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeOver()
+    {
+        fadeOver = true;
+    }
+}
