8237ac44e52eb1617476c4890de1614bb748badd
Unknown <kkinney2@student.fitchburgstate.edu>
2020-03-27 09:42:36 -0400

    GazeGrowths
    ** Some meta files for unused files and deleted/renamed things
    
    Separated GazeGrowth into an inheritable class so that each type has its own variables. Although, Red/Blue still needs to be specified in the inspector drop down. All other gazegrowths will be entirely based on which script is grabbed. This may be changed later for simplicity.

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Interactions/Headbutt/GazeGrowths/GazeGrowth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeGrowth : Interactable
+{
+    public GazeGrowthType growthType;
+    public GazeGrowth gg;
+
+    protected SphereCollider thisDetector;
+
+    protected GameController gameController;
+    public Animator animator;
+
+
+    private void Start()
+    {
+        gameController = GameController.Instance;
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+    }
+}
+
+public enum GazeGrowthType
+{
+    Blue, Red, Green, Yellow
+}
+
+public enum GG_Anim
+{
+    Gaze_Hit_Trigger, Gaze_Cry_Bool
+}
---
+++ The Land Under Rot _ Unity/Assets/Scripts/Interactions/Headbutt/GazeGrowths/Green_GazeGrowth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Green_GazeGrowth : GazeGrowth
+{
+    public bool isOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        animator.SetTrigger(GG_Anim.Gaze_Hit_Trigger.ToString());
+        animator.SetBool(GG_Anim.Gaze_Cry_Bool.ToString(), true);
+
+        isOpen = !isOpen;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Headbutt"))
+        {
+            Interact();
+        }
+    }
+}
