00689d607e1e15e24abf635f48f32740e8f5172d
Nick <ncazmay@student.fitchburgstate.edu>
2020-04-03 11:57:28 -0400

    Angel, Star, and Willow Mulch Prefabs

---
+++ The Land Under Rot _ Unity/Assets/Bottle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle : MonoBehaviour
+{
+    public enum Bottle_Type { BROWN, GREEN, YELLOW }
+    public Bottle_Type bottleType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
