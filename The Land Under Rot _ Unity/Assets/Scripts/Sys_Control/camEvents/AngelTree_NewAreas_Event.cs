488407d9bee735dcc1cab3b83b5fa33523c8e2fa
Nick <ncazmay@student.fitchburgstate.edu>
2020-04-06 18:47:54 -0400

    AngelTree Cam Event Update
    Script complete; testing now.

--- The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/camEvents/AngelTree_NewAreas_Event.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/camEvents/AngelTree_NewAreas_Event.cs
@@ -6,31 +6,116 @@ public class AngelTree_NewAreas_Event : Event_Type
 {
     public GameObject fruitEntranceBranches;
     public GameObject willowEntranceBranches;
-    public GameObject treaSeatBossBranch;
+    public GameObject treeSeatBossBranch;
 
-    [Range(1f,20f)]
+    Vector3 fruitStartPos;
+    Vector3 fruitLoweredPos;
+
+    Vector3 willowStartPos;
+    Vector3 willowLoweredPos;
+
+    Vector3 angelBossBranchStartPos;
+    Vector3 angelBossBranchLoweredPos;
+
+    [Space(5)]
+
+    [Header("Fruit Branch Parameters")]
+
+    [Range(1f, 20f)]
     public float fruitEntranceDelay;
+    [Range(1f, 20f)]
+    public float fruitEntranceLowerDist;
+    [Range(0.1f, 10.0f)]
+    public float dropSpeedFruit;
+
+    [Space(3)]
+
+    [Header("Willow Branch Parameters")]
+
     [Range(1f, 20f)]
     public float willowEntranceDelay;
     [Range(1f, 20f)]
-    public float treeSeatBossBranchDelay;
+    public float willowEntranceLowerDist;
+    [Range(0.1f, 10.0f)]
+    public float dropSpeedWillow;
+
+    [Space(3)]
+
+    [Header("Angel Boss Branch Parameters")]
 
     [Range(1f, 20f)]
-    public float fruitEntranceLowerDist;
-    [Range(1f, 20f)]
-    public float willowEntranceLowerDist;
+    public float treeSeatBossBranchDelay;
     [Range(1f, 20f)]
     public float treeSeatBossBranchLowerDist;
+    [Range(0.1f, 10.0f)]
+    public float dropSpeedAngelBoss;
+
+    [Space(3)]
+
+    private float dropRecorderFruit;
+    private float dropRecorderWillow;
+    private float dropRecorderAngelBoss;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dropRecorderFruit = dropSpeedFruit;
+        dropRecorderWillow = dropSpeedWillow;
+        dropRecorderAngelBoss = dropSpeedAngelBoss;
+
+
+        fruitStartPos = new Vector3(fruitEntranceBranches.transform.position.x, fruitEntranceBranches.transform.position.y, fruitEntranceBranches.transform.position.z);
+        fruitLoweredPos = new Vector3(fruitEntranceBranches.transform.position.x, fruitEntranceBranches.transform.position.y - fruitEntranceLowerDist, fruitEntranceBranches.transform.position.z);
+
+        willowStartPos = new Vector3(willowEntranceBranches.transform.position.x, willowEntranceBranches.transform.position.y, willowEntranceBranches.transform.position.z);
+        willowLoweredPos = new Vector3(willowEntranceBranches.transform.position.x, willowEntranceBranches.transform.position.y - willowEntranceLowerDist, willowEntranceBranches.transform.position.z);
+
+        angelBossBranchStartPos = new Vector3(treeSeatBossBranch.transform.position.x, treeSeatBossBranch.transform.position.y, treeSeatBossBranch.transform.position.z);
+        angelBossBranchLoweredPos = new Vector3(treeSeatBossBranch.transform.position.x, treeSeatBossBranch.transform.position.y - treeSeatBossBranchLowerDist, treeSeatBossBranch.transform.position.z);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void StartEvent()
     {
-        
+        base.StartEvent();
+
+        StartCoroutine(RevealNewAreas());
+    }
+
+    IEnumerator RevealNewAreas()
+    {
+        dropSpeedFruit = dropRecorderFruit;
+        dropSpeedWillow = dropRecorderWillow;
+        dropSpeedAngelBoss = dropRecorderAngelBoss;
+
+        float fruitIteration = dropSpeedFruit;
+        float willowIteration = dropSpeedWillow;
+        float angelBossIteration = dropSpeedAngelBoss;
+
+        yield return new WaitForSeconds(fruitEntranceDelay);
+
+        while (dropSpeedFruit < 1.0f)
+        {
+            fruitEntranceBranches.transform.position = Vector3.Lerp(fruitEntranceBranches.transform.position, fruitLoweredPos, dropSpeedFruit);
+            dropSpeedFruit += fruitIteration;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(willowEntranceDelay);
+
+        while (dropSpeedFruit < 1.0f)
+        {
+            willowEntranceBranches.transform.position = Vector3.Lerp(willowEntranceBranches.transform.position, willowLoweredPos, dropSpeedWillow);
+            dropSpeedWillow += willowIteration;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(treeSeatBossBranchDelay);
+
+        while (dropSpeedFruit < 1.0f)
+        {
+            treeSeatBossBranch.transform.position = Vector3.Lerp(treeSeatBossBranch.transform.position, angelBossBranchLoweredPos, dropSpeedAngelBoss);
+            dropSpeedAngelBoss += angelBossIteration;
+            yield return null;
+        }
     }
 }
