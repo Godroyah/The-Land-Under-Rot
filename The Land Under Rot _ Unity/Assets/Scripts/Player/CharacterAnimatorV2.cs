2b979784de86dc910469e7afa0d927e0201b5b36
Ryley Forwood <rforwood@student.fitchburgstate.edu>
2020-04-07 16:50:21 -0400

    Added seedling anims and set them up in new player v 5

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Player/CharacterAnimatorV2.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatorV2 : MonoBehaviour
+{
+    PlayerController playerController;
+    Rigidbody rb;
+    Animator animator;
+    float inputValue;
+    bool justMoved;
+
+    public float landDistance = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        rb = playerController.Rb;
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.enabled == true)
+            if (!playerController.StopPlayer)
+            {
+                #region Jumping
+
+                if (playerController.ShouldJump)
+                {
+                    animator.SetTrigger(CharAnimation.Jump_Start_Trigger.ToString());
+                }
+
+                //faster falling
+                if (/*rb.velocity.y < 0 && */!playerController.IsGrounded)
+                    animator.SetBool(CharAnimation.Is_Falling_Bool.ToString(), true);
+                //else if (rb.velocity.y > 0 && !playerController.ShouldJump)
+                //animator.SetBool("Airborne_Bool", true);
+
+
+
+                //if (!animator.GetBool("Airborne_Bool") && (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")))
+                //    animator.SetTrigger("Run_Button_Trigger");
+
+                if (!animator.GetBool(CharAnimation.Is_Falling_Bool.ToString()) && (Mathf.Abs(playerController.HorizontalInput) > 0.1f || Mathf.Abs(playerController.VerticalInput) > 0.1f))
+                {
+                    if (!justMoved)
+                    {
+                        animator.SetTrigger(CharAnimation.Run_Start_Trigger.ToString());
+                    }
+                    justMoved = true;
+
+                }
+                else
+                    justMoved = false;
+
+
+                #endregion
+
+
+                if ((Mathf.Abs(playerController.HorizontalInput) > 0.1f || Mathf.Abs(playerController.VerticalInput) > 0.1f))
+                    animator.SetBool(CharAnimation.Is_Walking_Bool.ToString(), true);
+                else
+                    animator.SetBool(CharAnimation.Is_Walking_Bool.ToString(), false);
+
+
+                if (playerController.ShouldHeadbutt && playerController.HeadbuttCoroutine == null)
+                {
+                    animator.SetTrigger(CharAnimation.Headbutt_Trigger.ToString());
+                }
+
+
+            }
+            else
+            {
+                animator.SetBool(CharAnimation.Is_Walking_Bool.ToString(), false);
+            }
+
+        if (playerController.IsGrounded)
+        {
+            animator.SetBool(CharAnimation.Is_Falling_Bool.ToString(), false);
+        }
+
+    }
+}
+
+public enum CharAnimation
+{
+    Headbutt_Trigger,
+    Run_Start_Trigger,
+    Jump_Start_Trigger,
+    Idle_Break_Trigger, // not set up yet
+    Is_Falling_Bool,
+    Is_Drowning_Bool, // not set up yet
+    Is_Walking_Bool,
+    Is_Sprinting_Bool // not set up yet
+}
+//animator.SetTrigger(CharAnimation.Headbutt_Trigger.ToString());
