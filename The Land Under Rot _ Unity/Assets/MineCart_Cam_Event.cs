96f2b9b64845b40bcabfb3b1b9bb132cb6f9279b
Nick <ncazmay@student.fitchburgstate.edu>
2020-04-24 10:59:10 -0400

    Mulch Bug fixed + continued work on Mine cart event

--- The Land Under Rot _ Unity/Assets/MineCart_Cam_Event.cs
+++ The Land Under Rot _ Unity/Assets/MineCart_Cam_Event.cs
@@ -12,12 +12,14 @@ public class MineCart_Cam_Event : Event_Type
     private int currentPoint;
     private float distToPoint;
     private Vector3 targetDirection;
-    public Vector3 tipDirection;
+    public Transform tipDirection;
     private Vector3 newDirection;
 
     bool started;
 
-    public Transform[] wayPoints;
+    //public Transform[] wayPoints;
+
+    public Transform wayPoint;
 
     public override void StartEvent()
     {
@@ -38,26 +40,39 @@ public class MineCart_Cam_Event : Event_Type
     {
         started = true;
         //AudioManager.Instance.
-        
-        for (int i = 0; i < wayPoints.Length; i++)
+
+        targetDirection = wayPoint.position - transform.position;
+        distToPoint = Vector3.Distance(transform.position, wayPoint.position);
+        while (distToPoint > 0.1f)
         {
-            targetDirection = wayPoints[i].position - transform.position;
-            distToPoint = Vector3.Distance(transform.position, wayPoints[i].position);
-            while (distToPoint > 0.1f)
+            distToPoint = Vector3.Distance(transform.position, wayPoint.position);
+            targetDirection = wayPoint.position - transform.position;
+            newDirection = Vector3.RotateTowards(transform.forward, targetDirection, turnSpeed * Time.deltaTime, 0.0f);
+            transform.position = Vector3.MoveTowards(transform.position, wayPoint.position, moveSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+
+            yield return null;
+        }
+
+        //    for (int i = 0; i < wayPoints.Length; i++)
+        //{
+        //    targetDirection = wayPoints[i].position - transform.position;
+        //    distToPoint = Vector3.Distance(transform.position, wayPoints[i].position);
+        //    while (distToPoint > 0.1f)
+        //    {
+        //        distToPoint = Vector3.Distance(transform.position, wayPoints[i].position);
+        //        targetDirection = wayPoints[i].position - transform.position;
+        //        newDirection = Vector3.RotateTowards(transform.forward, targetDirection, turnSpeed * Time.deltaTime, 0.0f);
+        //        transform.position = Vector3.MoveTowards(transform.position, wayPoints[i].position, moveSpeed * Time.deltaTime);
+        //        transform.rotation = Quaternion.LookRotation(newDirection);
+        //        yield return null;
+        //    }
+            if(transform.position == wayPoint.position)
             {
-                distToPoint = Vector3.Distance(transform.position, wayPoints[i].position);
-                targetDirection = wayPoints[i].position - transform.position;
-                newDirection = Vector3.RotateTowards(transform.forward, targetDirection, turnSpeed * Time.deltaTime, 0.0f);
-                transform.position = Vector3.MoveTowards(transform.position, wayPoints[i].position, moveSpeed * Time.deltaTime);
-                transform.rotation = Quaternion.LookRotation(newDirection);
-                yield return null;
-            }
-            if(i == wayPoints.Length && (transform.position == wayPoints[i].position))
-            {
-                Vector3.RotateTowards(transform.right, tipDirection, tipSpeed * Time.deltaTime, 0.0f);
+                Vector3.RotateTowards(transform.right, tipDirection.position, tipSpeed * Time.deltaTime, 0.0f);
             }
             //currentPoint = i;
-        }
+        //}
         //lizardAnim.SetTrigger("End_Lizard");
     }
 }
