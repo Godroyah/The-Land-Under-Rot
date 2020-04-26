182dcd33cba1834f6f5190c155e0cfc6d846be0e
Nick <ncazmay@student.fitchburgstate.edu>
2020-04-26 12:32:50 -0400

    WillowTree Illumination Event and Test Objects set up

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/camEvents/Willow_Illuminate_Event.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Willow_Illuminate_Event : Event_Type
+{
+    public Yellow_GazeGrowth[] yellowGazeGrowths;
+    public float dissipationDelay;
+
+    //private void Start()
+    //{
+        
+    //}
+
+    public override void StartEvent()
+    {
+        base.StartEvent();
+
+        StartCoroutine(PermaFade());
+    }
+
+
+    IEnumerator PermaFade()
+    {
+        foreach (Yellow_GazeGrowth gazeGrowth in yellowGazeGrowths)
+        {
+            gazeGrowth.duration = 0;
+            gazeGrowth.overrideDuration = true;
+
+            yield return new WaitForSeconds(dissipationDelay);
+
+            gazeGrowth.Interact();
+
+            yield return null;
+        }
+    }
+}
