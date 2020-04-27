cf523ea20ed46b8ce96ed62c77a77bf4ff44f648
daynaclaire <51805898+daynaclaire@users.noreply.github.com>
2020-04-26 21:38:30 -0400

    Added "Music" Script to Wwise Global so Music works in all scenes

---
+++ The Land Under Rot _ Unity/Assets/Scripts/Music.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour // Replace ClassName with whatever your class name is
+{
+    public static Music singleton; // ClassName here should match the class name above
+    private void Start()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (singleton != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        AkSoundEngine.PostEvent("Start_Game", gameObject); // Call whatever event starts the music
+    }
+}
