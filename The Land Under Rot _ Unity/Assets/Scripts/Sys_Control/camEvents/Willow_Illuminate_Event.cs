84be39bc8beb082134814a6b12884f8f2d8e635b
Nick <ncazmay@student.fitchburgstate.edu>
2020-04-30 17:37:58 -0400

    Willow Tree Illumination Event Updated + Colliders on Cave Windows and Cordyceps

--- The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/camEvents/Willow_Illuminate_Event.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/camEvents/Willow_Illuminate_Event.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Willow_Illuminate_Event : Event_Type
 {
     public Yellow_GazeGrowth[] yellowGazeGrowths;
+    public Event_Trigger[] yellowEvents;
     public float dissipationDelay;
 
     //private void Start()
@@ -22,8 +23,14 @@ public class Willow_Illuminate_Event : Event_Type
 
     IEnumerator PermaFade()
     {
+        //for(int i = 0; i < yellowEvents.Length; i++)
+        //{
+        //    yellowEvents[i].enabled = false;
+        //    yield return null;
+        //}
         foreach (Yellow_GazeGrowth gazeGrowth in yellowGazeGrowths)
         {
+            gazeGrowth.usesCamEvent = false;
             gazeGrowth.duration = 0;
             gazeGrowth.overrideDuration = true;
 
