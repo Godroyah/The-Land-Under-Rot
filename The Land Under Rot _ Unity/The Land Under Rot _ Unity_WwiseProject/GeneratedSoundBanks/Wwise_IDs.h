6f12967523bce7ae06863121ffb56afbc8fa8ebe
joshuaragoodman <61438305+joshuaragoodman@users.noreply.github.com>
2020-05-01 21:02:37 -0400

    Fixed Fruitful Forest Mulchant ambience same as treeseat fix created new event for return

--- The Land Under Rot _ Unity/The Land Under Rot _ Unity_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ The Land Under Rot _ Unity/The Land Under Rot _ Unity_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -27,6 +27,7 @@ namespace AK
         static const AkUniqueID INT_BOING = 3774898542U;
         static const AkUniqueID INT_CORDYCEPS = 261243135U;
         static const AkUniqueID MULCHANT = 2403460855U;
+        static const AkUniqueID RETURN_FRUITFUL_FOREST = 3336759787U;
         static const AkUniqueID RETURN_TREE_SEAT = 1526298514U;
         static const AkUniqueID ROOTFORD = 1989498786U;
         static const AkUniqueID START_BUTTON = 2674457354U;
