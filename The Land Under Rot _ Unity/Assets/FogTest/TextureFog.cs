e9d538e361f4401afb50fd838b7844137a783603
Colin Everton <36973753+Crimsilon@users.noreply.github.com>
2020-05-05 21:36:07 -0400

    fug

---
+++ The Land Under Rot _ Unity/Assets/FogTest/TextureFog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(TextureFogRenderer), PostProcessEvent.AfterStack, "Custom/TextureFog")]
+public sealed class TextureFog : PostProcessEffectSettings
+{
+    [Range(0f, 1f)]
+    public FloatParameter fogPower = new FloatParameter { value = 0.5f };
+    public FloatParameter skyboxOffset = new FloatParameter { value = 10f };
+
+    [Header("Depth")]
+    public FloatParameter distanceBlend = new FloatParameter { value = 10f };
+    [Range(0f, 10f)]
+    public FloatParameter nearPush = new FloatParameter { value = 10f };
+    public ColorParameter depthColor = new ColorParameter();
+    public BoolParameter debugDepth = new BoolParameter();
+
+   
+}
+
+public sealed class TextureFogRenderer : PostProcessEffectRenderer<TextureFog>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/TextureFog"));
+
+        sheet.properties.SetFloat("_FogPower", settings.fogPower);
+
+        sheet.properties.SetFloat("_SkyboxOffset", settings.skyboxOffset);
+
+        sheet.properties.SetFloat("_DistanceBlend", settings.distanceBlend);
+        sheet.properties.SetFloat("_NearPush", settings.nearPush);
+        sheet.properties.SetFloat("_DebugDepth", settings.debugDepth ? 1 : 0);
+
+        sheet.properties.SetColor("_FogColor", settings.depthColor);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ The Land Under Rot _ Unity/Assets/FogTest/TextureFog.shader
@@ -0,0 +1,53 @@
+﻿Shader "Hidden/Custom/TextureFog"
+{
+    HLSLINCLUDE
+
+        #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+        float _DistanceBlend;
+        float4 _FogColor;
+        float _NearPush;
+        float _FogPower;
+		float _DebugDepth;
+      
+        sampler2D _CameraDepthTexture;
+
+        
+
+        float4 Frag(VaryingsDefault i) : SV_Target
+        {
+            half depth = tex2D(_CameraDepthTexture, i.texcoord).r;
+            float4 screenCol = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+            
+            
+            float linearDepth = Linear01Depth(depth);
+            float4 depthCol = _FogColor;
+            float depthAlpha = saturate((linearDepth - _NearPush*.01) * _DistanceBlend);
+			if (_DebugDepth > .1)
+				return depthAlpha;
+			
+			if (depthAlpha >= _DistanceBlend)
+				depthAlpha = 0;
+				
+			depthCol = lerp(screenCol, depthCol ,  depthAlpha );
+            
+            return lerp(screenCol,  depthCol , saturate(_FogPower));
+        }
+
+    ENDHLSL
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            HLSLPROGRAM
+
+                #pragma vertex VertDefault
+                #pragma fragment Frag
+
+            ENDHLSL
+        }
+    }
+}
