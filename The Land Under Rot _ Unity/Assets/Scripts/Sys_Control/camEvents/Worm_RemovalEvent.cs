af603b542bf8adbb94a7e50b11b459342c1514d0
Nick <ncazmay@student.fitchburgstate.edu>
2020-05-06 10:32:24 -0400

    Worms don't send you back to the main menu anymore

--- The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/camEvents/Worm_RemovalEvent.cs
+++ The Land Under Rot _ Unity/Assets/Scripts/Sys_Control/camEvents/Worm_RemovalEvent.cs
@@ -74,11 +74,11 @@ public class Worm_RemovalEvent : Event_Type
         //Remove for next build
 
         //fadeAnim.SetTrigger("FadeOut");
-        thankYouPane.SetTrigger("ThankYou");
-        thankYouText.SetTrigger("ThankYou");
+        //thankYouPane.SetTrigger("ThankYou");
+        //thankYouText.SetTrigger("ThankYou");
 
-        yield return new WaitForSeconds(3.0f);
+        //yield return new WaitForSeconds(3.0f);
 
-        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(0);
     }
 }
