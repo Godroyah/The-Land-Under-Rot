c70f257b32f858ffc5efef1f6379c2d6466e3227
Unknown <kkinney2@student.fitchburgstate.edu>
2020-05-11 13:17:38 -0400

    Testing

---
+++ The Land Under Rot _ Unity/Assets/_CameraRotation/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public GameObject thingToRotate;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        if (thingToRotate == null)
+        {
+            thingToRotate = this.gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thingToRotate.transform.RotateAround(thingToRotate.transform.position, thingToRotate.transform.up, speed * Time.deltaTime);
+    }
+}
